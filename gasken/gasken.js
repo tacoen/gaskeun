8a6d6988cbb79004e567922dcad59814d39bb583
tacoen <tacoen@gmail.com>
2020-08-23 00:30:15 +0900

    layout and showcase

--- gasken/gasken.js
+++ gasken/gasken.js
@@ -6,18 +6,39 @@ function ahasClass(element, className) {
 }
 
 
-function hasClass(element, className) {
+function hasClass(className) {
 	var c = document.getElementsByClassName(className).length
     console.log(className,c)
-	return c > 0;
+	return (c > 0)
 }
 
 function deck_toper() {
 
-	var tt = hasClass('section','deck-top');
+	if (hasClass('deck-top')) {
+		var element = document.getElementsByClassName('deck-top');
+		document.body.style.marginTop = element[0].offsetHeight + 'px';
+		document.body.className += 'has-decktop'
+	}
 	
-	var d = document.getElementsByClassName('deck-top');
-	if (tt) {document.body.style.marginTop = d[0].offsetHeight + 'px';}
+	if (hasClass('ga-features')) {
+
+	var div = document.getElementById('g-feature');
+		if (! div.classList.contains('flush')) {
+			div.className += ' flush '
+		}
+	}
+
+	if (hasClass('ga-showcase')) {
+
+	var div = document.getElementById('g-showcase');
+		if (! div.classList.contains('flush')) {
+			div.className += ' flush '
+		}
+
+	}
+}
+	
+
 /*
 	var ta = hasClass('div','ga-features');
 	if (ta) {
@@ -35,7 +56,6 @@ function deck_toper() {
 		}
 	}
 */	
-}
 
 function deck_scroll() {
 	window.addEventListener('scroll', (event) => {
@@ -43,7 +63,7 @@ function deck_scroll() {
 		var e = document.getElementsByClassName('deck-top');
 		var h = e[0];
 		if (scroll > h.offsetHeight) {
-			h.className = 'deck-top beyond';
+			h.className = 'deck-top scrolled';
 		} else {
 			h.className = 'deck-top';
 		}

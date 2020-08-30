97c47894f7e3e7625620991bb6ea78d3225d4864
tacoen <tacoen@gmail.com>
2020-08-30 12:22:14 +0900

    native toc.js cleanup

--- js/tocbot/update-toc-scroll.js
+++
@@ -1,9 +0,0 @@
-module.exports = function updateTocScroll(options) {
-  var toc = document.querySelector(options.tocSelector)
-  if (toc && toc.scrollHeight > toc.clientHeight) {
-    var activeItem = toc.querySelector('.' + options.activeListItemClass)
-    if (activeItem) {
-      toc.scrollTop = activeItem.offsetTop
-    }
-  }
-}
